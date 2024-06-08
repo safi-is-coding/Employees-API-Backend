fcfd7181239675c1abdd103c7c9583680c05d55d
safi-is-coding <cse.mohammadsafi@gmail.com>
2024-06-09 00:14:00 +0530

    update UI

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const connectDB = require('./db/dbConnection.js')
 const employeeRoute = require("./routes/employees.route.js")
 const cors = require("cors")
 const express = require("express")
+const path = require('path');
 
 require('dotenv').config()
 const app = express()
@@ -10,6 +11,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("./public/images"))
+app.use(express.static(path.join(__dirname, 'frontend')));
+
 
 app.use(cors({
     origin: process.env.CORS_ORIGINS,
@@ -21,8 +24,19 @@ app.use(cors({
 app.use("/api/employees", employeeRoute)
 
 // main route
+app.get('/addEmployee', (req, res)=> {
+    // res.send('<h1>Welcome to the Employees API</h1>')
+    res.sendFile(path.join(__dirname, 'frontend', 'addEmp.html'));
+})
+
 app.get('/', (req, res)=> {
-    res.send('<h1>Welcome to the Employees API</h1>')
+    // res.send('<h1>Welcome to the Employees API</h1>')
+    res.sendFile(path.join(__dirname, 'frontend', 'viewEmp.html'));
+})
+
+app.get('/', (req, res)=> {
+    // res.send('<h1>Welcome to the Employees API</h1>')
+    res.sendFile(path.join(__dirname, 'frontend', 'singleEmp.html'));
 })
 
 
